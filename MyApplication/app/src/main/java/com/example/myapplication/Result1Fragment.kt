7c60904c355ba0594dea7ab9b4aa4d426299e5d6
leeseokchan00 <starsc1@naver.com>
2023-10-01 00:27:24 +0900

    fragment 추가

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Result1Fragment.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentResult1Binding
+
+
+class Result1Fragment : Fragment() {
+
+    lateinit var binding: FragmentResult1Binding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentResult1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/Result2Fragment.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentResult2Binding
+
+
+class Result2Fragment : Fragment() {
+
+    lateinit var binding: FragmentResult2Binding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentResult2Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
